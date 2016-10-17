18df2163a23e75a8a493dc109e305e97630db40d
Sam Brightman <sam.brightman@gmail.com>
2016-10-17 15:53:52 +0200

    Clean up fundamental stuff, add Solarized customizations

---
+++ .emacs.d/lisp/penvwrapper.el
@@ -0,0 +1,91 @@
+;;; penvwrapper.el --- a minimal penv.pl tool for Emacs
+
+;; Copyright (C) 2016 Sam Brightman
+
+;; Author: Sam Brightman <sam.brightman@gmail.com>
+;; URL: http://github.com/sambrightman/penvwrapper.el
+;; Version: 20161001
+;; Keywords: perl, virtualenv, virtualenvwrapper, penv
+;; Package-Requires: ((virtualenvwrapper "20160929.1113"))
+
+;;; Commentary:
+
+;; An anaemic Perl virtualenv tool for Emacs.  Emulates a little of the
+;; functionality of Doug Hellmann's
+;; [virtualenvwrapper](https://bitbucket.org/dhellmann/virtualenvwrapper/) but
+;; based on [penv.pl](https://github.com/jtopjian/penv).  Abuses
+;; [virtualenvwrapper.el](https://github.com/porterjamesj/virtualenvwrapper.el).
+;; See also https://github.com/sambrightman/penvwrapper for command-line version.
+
+
+;; TODO:
+;; pull out function for add/remove
+;; add other environment variables
+;; add other functions
+;; automatically load when launched
+;; tests
+
+;;; Code:
+
+(eval-when-compile
+  (require 'dash)
+  (require 's)
+  (require 'virtualenvwrapper))
+
+(defvar penv-location
+  (expand-file-name (or (getenv "PWORKON_HOME") "~/.pvirtualenvs/"))
+  "The location(s) of your Perl virtualenvs (penvs).
+This can be either a string, which indicates a single directory
+in which you keep all your virutalenvs, or a list of strings, in
+which case it specifies disparate locations in which all your
+virtualenvs are kept.")
+
+(defvar penv-libraries-dir
+  (file-name-as-directory (concat (file-name-as-directory "lib") "perl5"))
+  "The name of the directory containing libraries.  Is it system dependent?")
+
+;;;###autoload
+(defun penv-workon (&optional name)
+  "Switch to penv NAME.  Prompts for NAME if called interactively."
+  (interactive)
+  (let ((venv-location penv-location))
+    (venv-workon name)
+    (add-to-env-path-variable penv-libraries-dir "PERL5LIB")
+    (add-to-env-path-variable "" "PERL_LOCAL_LIB_ROOT")))
+
+;;;###autoload
+(defun penv-deactivate ()
+  "Deactivate the current penv."
+  (interactive)
+  (let ((venv-location penv-location))
+    (remove-from-env-path-variable penv-libraries-dir "PERL5LIB")
+    (remove-from-env-path-variable "" "PERL_LOCAL_LIB_ROOT")
+    (venv-deactivate)))
+
+(defun path-join (paths)
+  "Join list PATHS using `path-separator'."
+  (s-join path-separator paths))
+
+(defun path-split (paths)
+  "Split string PATHS using `path-separator'."
+  (s-split path-separator paths))
+
+(defun add-to-env-path-variable (subdir variable)
+  "Add SUBDIR of current virtual environment to environment path variable VARIABLE if not already present."
+  (let ((current-value (getenv variable)))
+    (unless (-contains? (path-split current-value) subdir)
+      (setenv variable (concat venv-current-dir
+                               subdir
+                               path-separator
+                               current-value)))))
+
+(defun remove-from-env-path-variable (subdir variable)
+  "Remove SUBDIR of current virtual environment from environment variable VARIABLE."
+  (let ((venv-executables-dir subdir))
+    (setenv variable
+            (path-join (venv-get-stripped-path
+                        (path-split (getenv variable)))))))
+
+(provide 'penvwrapper)
+
+;;; penvwrapper.el ends here
---
+++ .emacs.d/lisp/solarized-extra-definitions.el
@@ -0,0 +1,140 @@
+;;; solarized-extra-definitions.el --- Extra Solarized face definitions
+
+;;; Commentary:
+
+;;; Definitions that have not yet made it into https://github.com/sellout/emacs-color-theme-solarized
+;;; Macro to apply them, currently duplicated from the source.
+
+;;; Code:
+
+(defmacro solarized-with-color-variables (&rest body)
+  "`let' bind all colors available for use in `solarized' around BODY."
+  (declare (indent 0))
+  `(let ((bold        (if solarized-bold 'bold        'unspecified))
+         (bright-bold (if solarized-bold 'unspecified 'bold))
+         (underline   (if solarized-underline t 'unspecified))
+         (opt-under   'unspecified)
+         (italic      (if solarized-italic 'italic 'unspecified)))
+     (cond ((eq 'high solarized-contrast)
+            (let ((orig-base3 base3))
+              (rotatef base01 base00 base0 base1 base2 base3)
+              (setf base3 orig-base3)))
+           ((eq 'low solarized-contrast)
+            (setf back      base02
+                  opt-under t)))
+     (let ((bg-back   '(:background back))
+           (bg-base03 '(:background base03))
+           (bg-base02 '(:background base02))
+           (bg-base01 '(:background base01))
+           (bg-base00 '(:background base00))
+           (bg-base0 '(:background base0))
+           (bg-base1 '(:background base1))
+           (bg-base2 '(:background base2))
+           (bg-base3 '(:background base3))
+           (bg-green '(:background green))
+           (bg-yellow '(:background yellow))
+           (bg-orange '(:background orange))
+           (bg-red '(:background red))
+           (bg-magenta '(:background magenta))
+           (bg-violet '(:background violet))
+           (bg-blue '(:background blue))
+           (bg-cyan '(:background cyan))
+
+           (fg-base03 '(:foreground base03))
+           (fg-base02 '(:foreground base02))
+           (fg-base01 '(:foreground base01))
+           (fg-base00 '(:foreground base00))
+           (fg-base0 '(:foreground base0))
+           (fg-base1 '(:foreground base1))
+           (fg-base2 '(:foreground base2))
+           (fg-base3 '(:foreground base3))
+           (fg-green '(:foreground green))
+           (fg-yellow '(:foreground yellow))
+           (fg-orange '(:foreground orange))
+           (fg-red '(:foreground red))
+           (fg-magenta '(:foreground magenta))
+           (fg-violet '(:foreground violet))
+           (fg-blue '(:foreground blue))
+           (fg-cyan '(:foreground cyan))
+
+           (fmt-none   `())
+           (fmt-bold   `(:weight ,bold))
+           (fmt-bldi   `(:weight ,bold :slant ,italic))
+           (fmt-undr   `(                             :underline ,underline))
+           (fmt-undb   `(:weight ,bold                :underline ,underline))
+           (fmt-undi   `(              :slant ,italic :underline ,underline))
+           (fmt-uopt   `(                             :underline ,opt-under))
+           ;; FIXME: don’t hardcode the SRGB color names
+           (fmt-curl-red    `(                        :underline (:color "#dc322f" :style wave)))
+           (fmt-curl-yellow `(                        :underline (:color "#b58900" :style wave)))
+           (fmt-curl-magenta `(                       :underline (:color "#d33682" :style wave)))
+           (fmt-curl-cyan `(                          :underline (:color "#2aa198" :style wave)))
+           (fmt-ital   `(              :slant ,italic))
+           ;; FIXME: not quite the same
+           (fmt-stnd   `(                                                   :inverse-video t))
+           (fmt-revr   `(                                                   :inverse-video t))
+           (fmt-revb   `(:weight ,bold                                      :inverse-video t))
+           (fmt-revbb  `(:weight ,bright-bold                               :inverse-video t))
+           (fmt-revbbu `(:weight ,bright-bold         :underline ,underline :inverse-video t)))
+       ,@body)))
+
+(defun solarized-apply-definitions (definitions theme)
+  "Apply face DEFINITIONS to THEME."
+  (apply 'custom-theme-set-faces
+	 theme
+	 (mapcar (lambda (face) (apply 'create-face-spec face))
+		 definitions)))
+
+(defvar my/solarized-extra-definitions
+  (solarized-with-color-variables
+    `(;; cperl
+      (cperl-array-face (,@fg-blue))
+      (cperl-hash-face (,@fg-blue))
+      (cperl-nonoverridable-face (,@fg-magenta))
+      ;; magit
+      (magit-branch-current (,@fg-violet))
+      (magit-branch-local (,@fg-blue))
+      (magit-branch-remote (,@fg-green))
+      (magit-diff-added (:inherit diff-added))
+      (magit-diff-added-highlight (:inherit (highlight diff-added)))
+      (magit-diff-context (:inherit default))
+      (magit-diff-context-highlight (:inherit highlight))
+      (magit-diff-file-heading (,@fmt-bold :inherit default))
+      (magit-diff-file-heading-highlight (,@fmt-bold :inherit highlight))
+      (magit-diff-hunk-heading (,@fg-blue))
+      (magit-diff-hunk-heading-highlight (,@fg-blue :inherit highlight))
+      (magit-diff-removed (:inherit diff-removed))
+      (magit-diff-removed-highlight (:inherit (highlight diff-removed)))
+      (magit-diffstat-added (:inherit diff-added))
+      (magit-diffstat-removed (:inherit diff-removed))
+      (magit-dimmed (:inherit shadow))
+      (magit-filename (:inherit default))
+      (magit-hash (,@fg-red))
+      (magit-head (,@fg-cyan))
+      (magit-header-line (:inherit magit-section-title))
+      (magit-log-author (,@fg-cyan))
+      (magit-log-date (,@fg-magenta))
+      (magit-log-graph (,@fg-blue))
+      (magit-section-heading (,@fmt-bold :inherit magit-section-title))
+      (magit-section-heading-selection (,@fg-blue))
+      (magit-section-highlight (:inherit highlight))
+      (magit-section-secondary-heading (,@fg-base1))
+      (magit-tag (,@fg-yellow))
+      ;; hi-lock
+      (hi-yellow (,@fg-yellow ,@bg-back))
+      (hi-pink (,@fg-magenta ,@bg-back))
+      (hi-green (,@fg-green ,@bg-back))
+      (hi-blue (,@fg-blue ,@bg-back))
+      (hi-black-b (,@fmt-bold ,@fg-base1 ,@bg-back))
+      (hi-blue-b (,@fg-violet ,@bg-back))
+      (hi-red-b (,@fg-red ,@bg-back))
+      (hi-green-b (,@fg-cyan ,@bg-back))
+      (hi-black-hb (,@fmt-bold ,@fg-base1 ,@bg-base02))
+      ;; phi-search
+      (phi-search-match-face (:inherit match))
+      (phi-search-selection-face (:inherit isearch))
+      )))
+
+(provide 'solarized-extra-definitions)
+
+;;; solarized-extra-definitions.el ends here
