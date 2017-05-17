2c1abb7946ffbde3a347c0176be2f306b8e9fb99
Sam Brightman <sam.brightman@gmail.com>
2017-05-17 13:08:42 +0100

    Add skeleton my-tokens.el

---
+++ .emacs.d/lisp/my-tokens.el
@@ -0,0 +1,9 @@
+;;; my-tokens.el --- Tokens used for authentication to services
+;;; Commentary:
+;;
+;;; Code:
+
+;; (setq-default gh-auth-alist '(("github" :username "sambrightman" :token "")))
+
+(provide 'my-tokens)
+;;; my-tokens.el ends here
