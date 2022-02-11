cc90435a5871582452e21ffe6f42cafc1f6a30bd
Sam Brightman <sam.brightman@gmail.com>
2022-02-11 03:45:44 +0000

    Improve package init

--- .emacs.d/lisp/my-packaging.el
+++ .emacs.d/lisp/my-packaging.el
@@ -4,11 +4,11 @@
 ;;; Code:
 
 (setq-default package--init-file-ensured t) ;; removed in Emacs 27
-(setq load-prefer-newer t)
+(setq-default warning-suppress-log-types '((package reinitialization)))
 (require 'cask (my/join-path "~" ".cask" "cask.el"))
-;; cask and epl manually edited to manually (setq package--initialized nil) before (package-initialize)
+;; cask and epl can be edited to (setq package--initialized nil) before (package-initialize)
 (cask-initialize)
-;; pallet mode manually edited to (or package--initialized (package-initialize))
+;; pallet mode can be edited to (or package--initialized (package-initialize))
 (pallet-mode t)
 
 (provide 'my-packaging)
