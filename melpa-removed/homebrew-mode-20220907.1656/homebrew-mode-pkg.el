be6740b710bf4a83a78f7da6dd3955758f86ae27
Sam Brightman <sam.brightman@gmail.com>
2024-08-17 18:54:35 +0000

    Add packages removed from MELPA but still in-use

---
+++ melpa-removed/homebrew-mode-20220907.1656/homebrew-mode-pkg.el
@@ -0,0 +1,16 @@
+(define-package "homebrew-mode" "20220907.1656" "Minor mode for editing Homebrew formulae"
+  '((emacs "24.4")
+    (inf-ruby "2.4.0")
+    (dash "1.2.0"))
+  :commit "e32da1397ce176766e39c286861ef4c40d64bbf5" :authors
+  '(("Alex Dunn" . "dunn.alex@gmail.com"))
+  :maintainers
+  '(("Alex Dunn" . "dunn.alex@gmail.com"))
+  :maintainer
+  '("Alex Dunn" . "dunn.alex@gmail.com")
+  :keywords
+  '("languages" "homebrew" "brew" "ruby")
+  :url "https://github.com/dunn/homebrew-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ melpa-removed/pallet-20150512.702/pallet-pkg.el
@@ -0,0 +1,15 @@
+(define-package "pallet" "20150512.702" "A package management tool for Emacs, using Cask."
+  '((dash "2.10.0")
+    (s "1.9.0")
+    (f "0.17.1")
+    (cask "0.7"))
+  :commit "0e1ae11e1ebfe644cbf832df62ac2dbf6ecd0501" :authors
+  '(("Robert Dallas Gray"))
+  :maintainer
+  '("Robert Dallas Gray")
+  :keywords
+  '("elpa" "package")
+  :url "https://github.com/rdallasgray/pallet")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
